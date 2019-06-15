b56990766c1933c84ed44fb68bdb29c30496c9ec
JakedStevens <onegreentiger@gmail.com>
2019-06-15 11:54:24 -0500

    init package start from Osei with travis ci and eslint

--- controllers/api/burger.js
+++ controllers/api/burger.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-var db = require("../models");
+var db = require("../../models");
 // get route -> index
 router.get("/", function(req, res) {
   //do things here for other routes
