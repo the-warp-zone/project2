122746da7e637e662d1662a0b092118c7b00eb75
JakedStevens <onegreentiger@gmail.com>
2019-06-19 02:25:59 -0500

    starting testing canvasJS

--- client/src/components/MainContentComponents/GraphComponents/Chart.js
+++ client/src/components/MainContentComponents/GraphComponents/Chart.js
@@ -1,77 +1,90 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
+import React, {Component} from "react";
 import "./chart.css";
-import zoom from 'chartjs-plugin-zoom'
-import Hammer from 'hammerjs';
+import CanvasJSReact from '../../../canvasjs.react';
+import axios from 'axios';
 
-const Chart = props => {
- return (
-  <div className="chart">
-   <Line
-    data={props.data}
-    options={{
-     responsive: true,
-     title: {
-      display: props.displayTitle,
-      text: "Fast Travel Stocks: " + props.company,
-      fontSize: 20
-     },
-     legend: {
-      display: props.displayLegend,
-      position: props.legendPosition
-     },
-     plugins: {
-        zoom: {
-            // Container for pan options
-            pan: {
-                // Boolean to enable panning
-                enabled: true,
-     
-                // Panning directions. Remove the appropriate direction to disable
-                // Eg. 'y' would only allow panning in the y direction
-                mode: 'x',
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+
+
+class Chart extends Component {
+    state = {
+        dataPoints: []
+      }
+      componentDidMount() {
+        axios({
+            url: "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&apikey=6J7PBJ5E6ZV76KME&symbol=UBSFY"
+            })
+          .then(response => {
+            //const dataPoints = response.data['Weekly Time Series'];
+            console.log(response.data['Weekly Time Series']);
+            // this.setState({ dataPoints });
+          })
+      }
+    render() {
+		const options = {
+			animationEnabled: true,
+            exportEnabled: true,
+            interactivityEnabled: true,
+            zoomEnabled:true,
+            zoomType: "xy",
+            // width: 700,
+            //backgroundColor: "#F5DEB3"
+			theme: "light1", // "light1","light2", "dark1", "dark2"
+			title:{
+				text: "Fast Travel Stocks"
+			},
+			axisY: {
+				title: "Price",
+				includeZero: false,
+				prefix: "$"
+			},
+			axisX: {
+				title: "Date",
+				prefix: "",
+				interval: 1
+			},
+			data: [{
+                color: "green",
+				type: "line",
+				toolTipContent: "Week {x}: {y}%",
+                dataPoints: //this.state.dataPoints
                 
-            
-                // Function called once panning is completed
-                // Useful for dynamic data loading
-                onPan: function({chart}) { console.log(`I was panned!!!`); }
-            },
-     
-            // Container for zoom options
-            zoom: {
-                // Boolean to enable zooming
-                enabled: true,
-     
-                // Enable drag-to-zoom behavior
-                drag: true,
-     
-                // Drag-to-zoom rectangle style can be customized
-                // drag: {
-                // 	 borderColor: 'rgba(225,225,225,0.3)'
-                // 	 borderWidth: 5,
-                // 	 backgroundColor: 'rgb(225,225,225)'
-                // },
-     
-                // Zooming directions. Remove the appropriate direction to disable
-                // Eg. 'y' would only allow zooming in the y direction
-                mode: 'xy',
-     
-                
-                
-     
-                // Speed of zoom via mouse wheel
-                // (percentage of zoom on a wheel event)
-                speed: 0.1,
-     
-                // Function called once zooming is completed
-                // Useful for dynamic data loading
-                onZoom: function({Chart}) { console.log(`I was zoomed!!!`); }
-            }
-        }
-    }
-    }}
-   />
-  </div>
- );
-};
+                [
+					{ x: 1, y: 64 },
+					{ x: 2, y: 61 },
+					{ x: 3, y: 64 },
+					{ x: 4, y: 62 },
+					{ x: 5, y: 64 },
+					{ x: 6, y: 60 },
+					{ x: 7, y: 58 },
+					{ x: 8, y: 59 },
+					{ x: 9, y: 53 },
+					{ x: 10, y: 54 },
+					{ x: 11, y: 61 },
+					{ x: 12, y: 60 },
+					{ x: 13, y: 55 },
+					{ x: 14, y: 60 },
+					{ x: 15, y: 56 },
+					{ x: 16, y: 60 },
+					{ x: 17, y: 59.5 },
+					{ x: 18, y: 63 },
+					{ x: 19, y: 58 },
+					{ x: 20, y: 54 },
+					{ x: 21, y: 59 },
+					{ x: 22, y: 64 },
+					{ x: 23, y: 59 }
+				]
+			}]
+		}
+		
+		return (
+		<div className="chart">
+			{/* {this.state.dataPoints.map()} */}
+			<CanvasJSChart  options = {options} />
+		</div>
+		);
+	}
+}
+ 
 export default Chart;
