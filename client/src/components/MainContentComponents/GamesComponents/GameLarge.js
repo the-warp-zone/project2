17cc025ba57d44552e1d3272590772f13afca7be
JakedStevens <onegreentiger@gmail.com>
2019-06-20 16:06:07 -0500

    got games API in the structure we care about

--- client/src/components/MainContentComponents/GamesComponents/GameLarge.js
+++ client/src/components/MainContentComponents/GamesComponents/GameLarge.js
@@ -3,25 +3,45 @@ import axios from "axios";
  
 class GameLarge extends Component {
     state = {
-
+        gamesList: []
     }
     componentDidMount() {
         axios({ 
-        url: "https://api-v3.igdb.com/companies", 
+        url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/companies", 
         method: 'POST', 
         headers: { 
             'Accept': 'application/json', 
             'user-key': "c1c717a3e484c33bb482bdb7f9fb7eb4" 
         }, 
-        data: "fields logo.*,name,published.name,published.first_release_date; where name = \"Ubisoft\";" 
+        data: "fields name,published.name,published.first_release_date; sort popularity desc; where name = \"Ubisoft\";" 
     }) .then(response => {
-         console.log(response.data); 
+         console.log(response.data[0]['published'][0]['first_release_date']); 
+            
+            let obj = response.data[0]['published'];
+            let gamesList = [];
+            console.log(obj);
+            
+            for (var property in obj) {
+            let x = obj[property]["name"];
+            let y = obj[property]["first_release_date"];
+            // console.log(y);
+            // console.log(x);
+            gamesList.push([x,y])
+            
+        }
+        console.log(gamesList);
+        this.setState({ gamesList });
+
+
+
         }) .catch(err => { console.error(err); });
     }
 	render() {
 		return (
 		<div>
-            
+            <p>
+                {this.state.gamesList}
+            </p>
 		</div>
 		);
 	}
