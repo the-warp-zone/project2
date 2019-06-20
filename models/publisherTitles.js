aebd59446ef50e73a63221ff662b9f871cadbe76
JakedStevens <onegreentiger@gmail.com>
2019-06-20 14:21:22 -0500

    update to last commit

--- models/publisherTitles.js
+++ models/publisherTitles.js
@@ -1,7 +1,4 @@
-// Burger models
 
-// The burger has a burger_name attribute of type DataTypes.String
-// and a devoured attribute that is false by default
 
 module.exports = function(sequelize, DataTypes) {
   var Titles = sequelize.define("titles", {
