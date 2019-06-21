59b6ee907a9c8fa21022896fb27ead2f7672f04c
JakedStevens <onegreentiger@gmail.com>
2019-06-20 21:32:00 -0500

    rixed sorting and timestamp after hours, props caleb and alex

--- client/src/components/MainContentComponents/GamesComponents/GameLarge.js
+++ client/src/components/MainContentComponents/GamesComponents/GameLarge.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import axios from "axios";
- 
+import moment from "moment";
 class GameLarge extends Component {
     state = {
         gamesList: []
@@ -15,21 +15,36 @@ class GameLarge extends Component {
         }, 
         data: "fields name,published.name,published.first_release_date; sort popularity desc; where name = \"Ubisoft\";" 
     }) .then(response => {
-         console.log(response.data[0]['published'][0]['first_release_date']); 
+         //console.log(response.data[0]['published'][0]['first_release_date']); 
             
             let obj = response.data[0]['published'];
             let gamesList = [];
-            console.log(obj);
+            //console.log(obj);
             
             for (var property in obj) {
             let x = obj[property]["name"];
             let y = obj[property]["first_release_date"];
+            //let y = moment.unix(obj[property]["first_release_date"]).format('MMMM Do YYYY');
             // console.log(y);
             // console.log(x);
-            gamesList.push([x,y])
+        
+            gamesList.push([moment.unix(y).format('MMM DD YYYY'),x])
+            //gamesList.push([y,x]);
             
         }
-        console.log(gamesList);
+        
+        const result = gamesList.filter(title => title[0] !== "Invalid date");
+        
+        const result2 = result.filter(title => title[1] !== undefined);
+        //console.log(result);
+        //console.log(gamesList);
+
+        let sortedArray = result2.sort((a,b) => new moment(a[0], 'MMM DD YYYY') - new moment(b[0], 'MMM DD YYYY'));
+        console.log(sortedArray);
+        
+        
+
+
         this.setState({ gamesList });
 
 
