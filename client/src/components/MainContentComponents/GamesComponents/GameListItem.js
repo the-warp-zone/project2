891904f581ee7faf66c59da1f84eefd0bba55730
JakedStevens <onegreentiger@gmail.com>
2019-06-20 22:49:54 -0500

    have our ordered list of games writing to the page

---
+++ client/src/components/MainContentComponents/GamesComponents/GameListItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GameListItem = props => {
+    return (
+        <div><li>Release Date: {props.date}, Title: {props.title}</li></div>
+        );
+    
+}
+
+
+export default GameListItem;
+
+
+
