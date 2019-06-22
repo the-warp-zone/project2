5a7b9626534fa9c1bd6d47e139a46387ce5843bb
JakedStevens <onegreentiger@gmail.com>
2019-06-22 02:18:25 -0500

    style changes for small and large card, added functionality

--- client/src/components/MainContentComponents/GamesComponents/LargeCard.js
+++ client/src/components/MainContentComponents/GamesComponents/LargeCard.js
@@ -1,45 +1,49 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import TableCell from '@material-ui/core/TableCell';
-import Paper from '@material-ui/core/Paper';
-import { AutoSizer, Column, Table } from 'react-virtualized';
-
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { withStyles } from "@material-ui/core/styles";
+import TableCell from "@material-ui/core/TableCell";
+import Paper from "@material-ui/core/Paper";
+import { AutoSizer, Column, Table } from "react-virtualized";
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   flexContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    boxSizing: 'border-box',
+    display: "flex",
+    alignItems: "center",
+    boxSizing: "border-box"
+  },
+  margin:{
+    margin: theme.spacing(1)
   },
   tableRow: {
-    cursor: 'pointer',
+    cursor: "pointer"
   },
   tableRowHover: {
-    '&:hover': {
-      backgroundColor: theme.palette.grey[300],
-    },
+    "&:hover": {
+      backgroundColor: theme.palette.grey[300]
+    }
   },
   tableCell: {
-    flex: 1,
+    flex: 1
   },
   noClick: {
-    cursor: 'initial',
-  },
+    cursor: "initial"
+  }
 });
 
-class MuiVirtualizedTable extends React.PureComponent  {
+class MuiVirtualizedTable extends React.PureComponent {
   static defaultProps = {
     headerHeight: 48,
     rowHeight: 48
   };
-  
+
   getRowClassName = ({ index }) => {
     const { classes, onRowClick } = this.props;
+    // console.log(this.props.classes);
 
     return clsx(classes.tableRow, classes.flexContainer, {
-      [classes.tableRowHover]: index !== -1 && onRowClick != null,
+      [classes.tableRowHover]: index !== -1 && onRowClick != null
     });
   };
 
@@ -47,50 +51,71 @@ class MuiVirtualizedTable extends React.PureComponent  {
     const { columns, classes, rowHeight, onRowClick } = this.props;
     return (
       <TableCell
+    
         component="div"
         className={clsx(classes.tableCell, classes.flexContainer, {
-          [classes.noClick]: onRowClick == null,
+          [classes.noClick]: onRowClick == null
         })}
         variant="body"
         style={{ height: rowHeight }}
-        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
+        align={
+          (columnIndex != null && columns[columnIndex].numeric) || false
+            ? "right"
+            : "left"
+        }
       >
         {cellData}
       </TableCell>
+      
     );
   };
 
   headerRenderer = ({ label, columnIndex }) => {
     const { headerHeight, columns, classes } = this.props;
-    
+
     return (
+      
       <TableCell
         component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
+        className={clsx(
+          classes.tableCell,
+          classes.flexContainer,
+          classes.noClick
+        )}
         variant="head"
         style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
+        align={columns[columnIndex].numeric || false ? "right" : "left"}
       >
+        
         <span>{label}</span>
       </TableCell>
     );
   };
-  
+
   render() {
-    
     const { classes, columns, ...tableProps } = this.props;
     return (
-      <AutoSizer>
+      <AutoSizer >
+        
         {({ height, width }) => (
-          <Table height={height} width={width} {...tableProps} rowClassName={this.getRowClassName}>
+          
+        <Table
+
+            height={height}
+            width={width}
+            {...tableProps}
+            rowClassName={this.getRowClassName}
+          >
             {columns.map(({ dataKey, ...other }, index) => {
               return (
+                
                 <Column
+                
                   key={dataKey}
                   headerRenderer={headerProps =>
                     this.headerRenderer({
                       ...headerProps,
-                      columnIndex: index,
+                      columnIndex: index
                     })
                   }
                   className={classes.flexContainer}
@@ -101,8 +126,10 @@ class MuiVirtualizedTable extends React.PureComponent  {
               );
             })}
           </Table>
-        )}
+          )}
+          
       </AutoSizer>
+      
     );
   }
 }
@@ -112,55 +139,61 @@ MuiVirtualizedTable.propTypes = {
   columns: PropTypes.arrayOf(PropTypes.object).isRequired,
   headerHeight: PropTypes.number,
   onRowClick: PropTypes.func,
-  rowHeight: PropTypes.number,
+  rowHeight: PropTypes.number
 };
+
 
 const VirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
 
-// ---DATA HERE FROM GAMELARGE.js-----
-
-
-
-
-//const gameList = this.getData();
-
-//console.log(gameList);
-
-
-
-const ReactVirtualizedTable = ({data}) => {
+const ReactVirtualizedTable = ({ data }) => {
+  
   const gameList = data;
   function createData(id, date, title) {
-    return { id, date, title};
+    return { id, date, title };
   }
-  
+
   const rows = [];
-  
+
   for (let i = 0; i < gameList.length; i += 1) {
     rows.push(createData(i, gameList[i][0], gameList[i][1]));
   }
-  
-
-
+  const bStyle = {
+    marginRight: "1%",
+    marginTop: "1%",
+    float: "right",
+    background: 'linear-gradient(45deg, #0a9df1 , #62058d )',
+    color: "white"
+  }
   return (
-    <Paper style={{ height: 600, width: '20%', float: "right", margin: '15%' }}>
+    
+    <Paper style={{ 
+      height: 800,
+      width: "20%", 
+      // background: 'linear-gradient(45deg, #0a9df1, #62058d)', 
+      float: "right", 
+      marginTop: "8%", 
+      marginRight: "25%" }}>
       <VirtualizedTable
         rowCount={rows.length}
+        
         rowGetter={({ index }) => rows[index]}
         columns={[
           {
             width: 200,
-            label: 'Release Date',
-            dataKey: 'date',
+            label: "Release Date",
+            dataKey: "date"
           },
           {
             width: 400,
-            label: 'Game Title',
-            dataKey: 'title',
+            label: "Game Title",
+            dataKey: "title"
           }
         ]}
       />
+      <Button variant="contained" size="small" color="third" style={bStyle}>
+          Close
+        </Button>
     </Paper>
   );
-}
+};
 export default ReactVirtualizedTable;
