752e251698227811e61d00faa3cca36a03f19f78
JakedStevens <onegreentiger@gmail.com>
2019-06-22 16:12:10 -0500

    got state variables set with dummy data for prices

--- client/src/components/MainContentComponents/Tickercomponents/TickerMain.js
+++ client/src/components/MainContentComponents/Tickercomponents/TickerMain.js
@@ -1,30 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import "./ticker.css";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(3, 2)
-  }
-}));
 
-const Ticker = () => {
-  const classes = useStyles();
-
-  return (
-    <div>
-      <Paper className={classes.root}>
-        <Typography variant="h5" component="h3">
-          Nintendo(NTDOY): "" | Activision(ATVI): "" | Electronic Arts(EA): ""
+class Ticker extends Component {
+  state = {
+    price: {
+      NTDOY: 1,
+      ATVI: 2,
+      EA: 3,
+      TTWO: 4,
+      SNE: 5
+    }
+  };
+   render(){
+    
+    return (
+      
+    <div className="ticker">
+      <Paper style={{
+        padding: 10,
+        }}>
+        <Typography variant="h5" component="h3" style={{textAlign:"center"}}>
+          Nintendo(NTDOY): ${this.state.price.NTDOY} | Activision(ATVI): ${this.state.price.ATVI} | Electronic Arts(EA): ${this.state.price.EA}
         </Typography>
-        <Typography variant="h5" component="h3">
-          Take Two Interactive(TTWO): "" | Sony Interactive Entertainment(SNE):
-          ""
+        <Typography variant="h5" component="h3" style={{textAlign:"center"}}>
+          Take Two Interactive(TTWO): ${this.state.price.TTWO} | Sony Interactive Entertainment(SNE): ${this.state.price.SNE}
         </Typography>
       </Paper>
     </div>
   );
 };
+}
+  
+       
+     
+    
+  
+  
+  
+  
 
 export default Ticker;
