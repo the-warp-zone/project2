5fe2d653b36c9f6562841f96fb32d577ec364e59
JakedStevens <onegreentiger@gmail.com>
2019-06-22 17:20:23 -0500

    have axios for tickers, need to reset state and use sequelize

--- client/src/components/MainContentComponents/Tickercomponents/TickerMain.js
+++ client/src/components/MainContentComponents/Tickercomponents/TickerMain.js
@@ -2,45 +2,107 @@ import React, { Component } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import axios from "axios";
+import moment from "moment";
 import "./ticker.css";
 
 
 class Ticker extends Component {
   state = {
-    price: {
-      NTDOY: 1,
-      ATVI: 2,
+      Nintendo: 1,
+      Activision: 2,
       EA: 3,
-      TTWO: 4,
-      SNE: 5
-    }
-  };
-   render(){
+      TakeTwo: 4,
+      Sony: 5
     
-    return (
+  };
+  componentDidMount() {
+    axios({
+      url:
+        "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&apikey=6J7PBJ5E6ZV76KME&symbol=NTDOY"
+    }).then(response => {
       
+      let obj = response.data["Weekly Time Series"];
+      let dateObj = Object.keys(obj);
+      let todaysDateKey = dateObj[0];
+      let todaysPrice = response.data["Weekly Time Series"][todaysDateKey];
+      let todaysClosePrice = todaysPrice['4. close']
+     
+      console.log("Nintendo $" + todaysClosePrice)
+
+    });
+    axios({
+      url:
+        "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&apikey=6J7PBJ5E6ZV76KME&symbol=ATVI"
+    }).then(response => {
+      
+      let obj = response.data["Weekly Time Series"];
+      let dateObj = Object.keys(obj);
+      let todaysDateKey = dateObj[0];
+      let todaysPrice = response.data["Weekly Time Series"][todaysDateKey];
+      let todaysClosePrice = todaysPrice['4. close']
+     
+      console.log("Activision $" + todaysClosePrice)
+
+    });
+    axios({
+      url:
+        "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&apikey=6J7PBJ5E6ZV76KME&symbol=EA"
+    }).then(response => {
+      
+      let obj = response.data["Weekly Time Series"];
+      let dateObj = Object.keys(obj);
+      let todaysDateKey = dateObj[0];
+      let todaysPrice = response.data["Weekly Time Series"][todaysDateKey];
+      let todaysClosePrice = todaysPrice['4. close']
+     
+      console.log("EA $" + todaysClosePrice)
+
+    });
+    axios({
+      url:
+        "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&apikey=6J7PBJ5E6ZV76KME&symbol=TTWO"
+    }).then(response => {
+      
+      let obj = response.data["Weekly Time Series"];
+      let dateObj = Object.keys(obj);
+      let todaysDateKey = dateObj[0];
+      let todaysPrice = response.data["Weekly Time Series"][todaysDateKey];
+      let todaysClosePrice = todaysPrice['4. close']
+     
+      console.log("TAKE2 $" + todaysClosePrice)
+
+    });
+    axios({
+      url:
+        "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&apikey=6J7PBJ5E6ZV76KME&symbol=SNE"
+    }).then(response => {
+      
+      let obj = response.data["Weekly Time Series"];
+      let dateObj = Object.keys(obj);
+      let todaysDateKey = dateObj[0];
+      let todaysPrice = response.data["Weekly Time Series"][todaysDateKey];
+      let todaysClosePrice = todaysPrice['4. close']
+     
+      console.log("Sony $" + todaysClosePrice)
+
+    });
+  }
+   render(){
+    return (
     <div className="ticker">
       <Paper style={{
         padding: 10,
         }}>
         <Typography variant="h5" component="h3" style={{textAlign:"center"}}>
-          Nintendo(NTDOY): ${this.state.price.NTDOY} | Activision(ATVI): ${this.state.price.ATVI} | Electronic Arts(EA): ${this.state.price.EA}
+          Nintendo(NTDOY): ${this.state.Nintendo} | Activision(ATVI): ${this.state.Activision} | Electronic Arts(EA): ${this.state.EA}
         </Typography>
         <Typography variant="h5" component="h3" style={{textAlign:"center"}}>
-          Take Two Interactive(TTWO): ${this.state.price.TTWO} | Sony Interactive Entertainment(SNE): ${this.state.price.SNE}
+          Take Two Interactive(TTWO): ${this.state.TakeTwo} | Sony Interactive Entertainment(SNE): ${this.state.Sony}
         </Typography>
       </Paper>
     </div>
   );
 };
 }
-  
-       
-     
-    
-  
-  
-  
-  
-
 export default Ticker;
