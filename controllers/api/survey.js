c5afe54fedb3607596760a8704de0d13c798171e
Caleb Rose <ctrose17@gmail.com>
2019-06-29 10:57:08 -0500

    Patch: Fixing DB Models
    Fixed an issue where one my branches pushed more than it should've.
    
    - Survey model pushed a merge conflict that was coded in.
    - Removed merge code in order to fix Heroku issue

--- controllers/api/survey.js
+++ controllers/api/survey.js
@@ -16,23 +16,7 @@ router.get("/results/:publisher_name", function(req, res) {
     res.json(dbSurvey);
   });
 });
-<<<<<<< HEAD
-router.put("/update/:publisher_name", function(req, res) {
-  // replace old function with sequelize function
 
-  db.Survey.update(
-    {
-      yes_count: req.body.yes_count,
-      no_count: req.body.no_count
-    },
-    {
-      where: {
-        publisher_name: req.params.publisher_name
-      }
-    }
-  ).then(function(dbSurvey) {
-    console.log(dbSurvey);
-=======
 router.post("/create/:publisher_name", function(req, res) {
   db.Survey.create({
     publisher_name: req.params.publisher_name,
@@ -40,7 +24,6 @@ router.post("/create/:publisher_name", function(req, res) {
     no_count: req.body.no_count
   }).then(function(dbSurvey) {
     //console.log(dbSurvey);
->>>>>>> abafb631373bda47b843d5b2e4710c0603e54807
     res.json(dbSurvey);
   });
 });
